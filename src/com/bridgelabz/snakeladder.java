43c39503ba840a34cc6522caa1fc2dc65ea87103
lokeshmk25 <lokeshmk2211@gmail.com>
2021-08-06 21:27:22 +0530

    Added:random value

--- src/com/bridgelabz/snakeladder.java
+++ src/com/bridgelabz/snakeladder.java
@@ -1,10 +1,17 @@
 package com.bridgelabz;
 import java.util.Scanner;
+import java.util.Random;
 public class snakeladder {
     public static void main(String[] args) {
         int startingposition = 0;
         System.out.println("enter your name:");
         Scanner name = new Scanner(System.in);
         String playername = name.next();
+
+        Random dice= new Random();
+        int number;
+        number = dice.nextInt(6);
+        System.out.println("number is "+number);
+
     }
 }
