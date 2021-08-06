5afba7cff5a260c0bee356917930d4bfb89a5101
lokeshmk25 <lokeshmk2211@gmail.com>
2021-08-06 23:47:00 +0530

    Added: position of th dice

--- src/com/bridgelabz/snakeladder.java
+++ src/com/bridgelabz/snakeladder.java
@@ -6,6 +6,7 @@ public class snakeladder {
 
     public static void main(String[] args) {
         int startposition = 0;
+        int dieRoll =0;
         System.out.println("enter your name:");
         Scanner name = new Scanner(System.in);
         String playername = name.next();
@@ -13,6 +14,9 @@ public class snakeladder {
             Random dice = new Random();
             int dicenumber;
             dicenumber = dice.nextInt(6);
+            if(dicenumber<=6) {
+                dieRoll = dieRoll + 1;
+            }
             System.out.println("number is " + dicenumber);
             int checkoption;
             checkoption = dice.nextInt(3);
@@ -27,16 +31,18 @@ public class snakeladder {
                 case 1:
                     System.out.println("ladder :" + "+" + dicenumber);
                     startposition += dicenumber;
+                    System.out.println("current position is" +startposition);
                     break;
                 default:
                     System.out.println("ladder :" + "-" + dicenumber);
                     startposition -= dicenumber;
+                    System.out.println("current position is" +startposition);
 
                     if (startposition < 0) {
                         startposition = 0;
                         System.out.println("star again");
                     }
-            }
+            }System.out.println("number of times dice is rolled=" +dieRoll);
             }
         }System.out.println( playername+ "wins the game");
     }
