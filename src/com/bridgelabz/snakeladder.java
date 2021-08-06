0de2e1016c18af3cd1faabe7bc6c738c07c1c51a
lokeshmk25 <lokeshmk2211@gmail.com>
2021-08-06 22:39:36 +0530

    Added:winning position

--- src/com/bridgelabz/snakeladder.java
+++ src/com/bridgelabz/snakeladder.java
@@ -2,32 +2,36 @@ package com.bridgelabz;
 import java.util.Scanner;
 import java.util.Random;
 public class snakeladder {
+    public static final int WINNING_POSITION = 100;
+
     public static void main(String[] args) {
         int startposition = 0;
         System.out.println("enter your name:");
         Scanner name = new Scanner(System.in);
         String playername = name.next();
+        while (WINNING_POSITION > startposition) {
+            Random dice = new Random();
+            int dicenumber;
+            dicenumber = dice.nextInt(6);
+            System.out.println("number is " + dicenumber);
+            int checkoption;
+            checkoption = dice.nextInt(3);
+            System.out.println("number is " + checkoption);
 
-        Random dice= new Random();
-        int dicenumber;
-        dicenumber = dice.nextInt(6);
-        System.out.println("number is "+dicenumber);
-        int checkoption;
-        checkoption = dice.nextInt(3);
-        System.out.println("number is "+checkoption);
-        switch (checkoption) {
-            case 0:
-                System.out.println("Noplay");
-                startposition +=0;
-                break;
-            case 1:
-                System.out.println("ladder :"+ "+" +dicenumber);
-                startposition += dicenumber;
-                break;
-            default:
-                System.out.println("ladder :"+"-" +dicenumber);
-                startposition -= dicenumber;
-                break;
-        }
+            switch (checkoption) {
+                case 0:
+                    System.out.println("Noplay");
+                    startposition += 0;
+                    break;
+                case 1:
+                    System.out.println("ladder :" + "+" + dicenumber);
+                    startposition += dicenumber;
+                    break;
+                default:
+                    System.out.println("ladder :" + "-" + dicenumber);
+                    startposition -= dicenumber;
+                    break;
+            }
+        }System.out.println( playername+ "wins the game");
     }
 }
