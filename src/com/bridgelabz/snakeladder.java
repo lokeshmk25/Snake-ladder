dec043b9c8ed352088cf46867a31759b1c4a60d0
lokeshmk25 <lokeshmk2211@gmail.com>
2021-08-06 22:20:00 +0530

    Added:check options

--- src/com/bridgelabz/snakeladder.java
+++ src/com/bridgelabz/snakeladder.java
@@ -3,15 +3,31 @@ import java.util.Scanner;
 import java.util.Random;
 public class snakeladder {
     public static void main(String[] args) {
-        int startingposition = 0;
+        int startposition = 0;
         System.out.println("enter your name:");
         Scanner name = new Scanner(System.in);
         String playername = name.next();
 
         Random dice= new Random();
-        int number;
-        number = dice.nextInt(6);
-        System.out.println("number is "+number);
-
+        int dicenumber;
+        dicenumber = dice.nextInt(6);
+        System.out.println("number is "+dicenumber);
+        int checkoption;
+        checkoption = dice.nextInt(3);
+        System.out.println("number is "+checkoption);
+        switch (checkoption) {
+            case 0:
+                System.out.println("Noplay");
+                startposition +=0;
+                break;
+            case 1:
+                System.out.println("ladder :"+ "+" +dicenumber);
+                startposition += dicenumber;
+                break;
+            default:
+                System.out.println("ladder :"+"-" +dicenumber);
+                startposition -= dicenumber;
+                break;
+        }
     }
 }
